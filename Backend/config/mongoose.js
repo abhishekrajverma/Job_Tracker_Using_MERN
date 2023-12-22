8b847bedc6adf336c0e414cf5f7df15dda1eed68
Abhishek Raj <abhishekr359@gmail.com>
2023-12-22 19:53:29 +0530

    setup the database MongoDB

---
+++ Backend/config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+main()
+.then(() => console.log("Connected to Database :: MongoDB"))
+.catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/jobportal");
+
+}
