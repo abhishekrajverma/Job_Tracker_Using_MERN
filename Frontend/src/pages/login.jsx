48cf7129325bb2c7365138c61b145976eb6e2db2
Abhishek Raj <abhishekr359@gmail.com>
2024-03-11 10:39:00 +0530

    fix error massage not does not go even user refresh the page

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -15,6 +15,8 @@ export default function SignIn() {
     const { loading, error } = useSelector((state) => state.user) // get loading and error from redux store 
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [updateError, setUpdateError] = useState(false); // update success state 
+
 
     const handleChange = (e) => {
         setFormData({
@@ -48,6 +50,7 @@ export default function SignIn() {
         } catch (err) {
             if (err.response) {
                 dispatch(signInFailure(err.response.data.message))
+                setUpdateError(true); // set update error to true
                 return;
             }
         }
@@ -94,7 +97,7 @@ export default function SignIn() {
                         <span className="btn btn-xs">Sign Up</span>
                     </Link>
                 </div>
-                {error && <div role="alert" className="alert alert-error">
+                {error && updateError && <div role="alert" className="alert alert-error">
                     <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                     <span>{error}</span>
                 </div>}
