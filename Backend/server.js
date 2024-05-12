641c000cce5be023ed15115ae4540e435267421c
Abhishek Raj <abhishekr359@gmail.com>
2024-05-12 14:26:03 +0530

    hmm

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(Express.static(path.join(__dirname, "/Frontend/dist")));
 
 // Serve index.html file for all routes except the ones defined above (API routes) 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, "/Frontend/dist/index.html"));
 });
 
 // Error handling middleware
