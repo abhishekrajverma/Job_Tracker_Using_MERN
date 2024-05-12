bf12fc042cd41b425b0b8af1e28fe5c269973a91
Abhishek Raj <abhishekr359@gmail.com>
2024-05-12 14:37:50 +0530

    solve typo

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // Import necessary components and modules
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Signup from "./pages/SignUp";
+import Signup from "./pages/SignUp.jsx";
 import Login from "./pages/Login";
 import About from "./pages/About";
 import { ListingPrivateRoute, PrivateRoute } from "./components/PrivateRoute.jsx";
